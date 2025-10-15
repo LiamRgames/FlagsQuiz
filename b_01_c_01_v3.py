3a70630a95fe5691a325ac04ec00b05e4a76d86f
LiamRgames <124933116+LiamRgames@users.noreply.github.com>
2025-10-16 11:28:19 +1300

    Component 1 Version 3

---
+++ b_01_c_01_v3.py
@@ -0,0 +1,15 @@
+#Select Difficulty Component
+
+response = ''
+difficulties = ["easy","medium","hard"]
+def select_difficulty(user_difficulty):
+    while True:
+        global response
+        response = input("What Difficulty would you like to play?\n").lower()
+        if response in difficulties:
+            return response
+        else:
+            print("That response is invalid. Please try again.")
+while True:
+    response = select_difficulty(response)
+    print(f"You chose {response}")
